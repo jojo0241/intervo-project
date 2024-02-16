6a02d7bb85c220bd017352917caa61987af9cf58
jojo0241 <joelndri518@gmail.com>
2024-02-16 15:25:39 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import QuiSommesNous from './QuiSommesNous';
+import NosValeurs from './NosValeurs';
+import Newsletter from './Newsletter';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+     <QuiSommesNous></QuiSommesNous>
+     <NosValeurs></NosValeurs>
+     <Newsletter></Newsletter>
+
+
     </div>
   );
 }
